56a7bc3347cf461fa663aedda87e1b4e0cb1395f
endorphin82 <endorphin82@gmail.com>
2019-04-08 09:50:05 +0300

    007-add_hot-reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const path = require("path");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "/public"),
+    path: path.join(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: "./public"
+    // publicPath: "./public"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        // use: ["react-hot-loader/webpack"],
         loader: "babel-loader",
+        include: path.join(__dirname, 'src'),
         options: { babelrcRoots: [".", "../"] } //MAGIC for @babel/plugin-proposal-class-properties
       }
     ]

f9f9fb82c2144a40786cebef5deca9f3ddfd5582
endorphin82 <endorphin82@gmail.com>
2019-04-14 15:23:27 +0300

    027-add_immutable

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { commentSelector, commentSelectorFactory } from "../selectors";
+import { commentSelectorFactory } from "../selectors";
 
 const Comment = ({ comment }) => {
   return (
--- src/decorators/toggleOpen.js
+++ src/decorators/toggleOpen.js
@@ -2,7 +2,7 @@ import React, { Component as ReactComponent } from "react";
 
 export default (OriginalComponent) => class WrappedComponent extends ReactComponent {
   state = {
-    isOpen: true
+    isOpen: false
   };
 
   render() {
